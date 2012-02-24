b7404f35dd1b7b8ca3887b73625d23b98533c175
Matthias Studer <matthias.studer@unige.ch>
2012-02-24 00:00:00 +0000

    version 1.0

---
+++ R/clusterquality.R
@@ -0,0 +1,156 @@
+wcClusterQuality <- function(diss, clustering, weights=NULL){
+	if (inherits(diss, "dist")) {
+		isdist <- TRUE
+		nelements <- attr(diss, "Size")
+	}else if(is.matrix(diss)){
+		isdist <- FALSE
+		nelements <- nrow(diss)
+		if(ncol(diss)!=nelements){
+			stop("[!] diss should be a squared matrix or a dist object.")
+		}
+	} else {
+		stop("[!] diss should be a squared matrix or a dist object.")
+	}
+	if(is.null(weights)){
+		weights <- as.double(rep(1.0, nelements))
+	}
+	clusterF <- factor(clustering)
+	if(nlevels(clusterF) < 2){
+		stop("[!] The clustering should have at least two different values.")
+	}
+	clustering <- as.integer(as.integer(clusterF)-1)
+	if(length(clustering)!=nelements|| length(weights)!=nelements){
+		stop("[!] different number of elements in diss, clustering and/or weights arguments.")
+	}
+	
+	ncluster <- max(clustering)+1
+	cq <- .Call(wc_RClusterQual, diss, clustering, as.double(weights), as.integer(ncluster), as.integer(isdist), as.integer(0))
+	names(cq[[1]]) <-c("PBC", "HG", "HGSD", "ASW", "ASWw", "CH", "R2", "CHsq", "R2sq", "HC")
+	dim(cq[[2]]) <- c(nlevels(clusterF), 2)
+	
+	rownames(cq[[2]]) <-levels(clusterF)
+	colnames(cq[[2]]) <- c("ASW", "ASWw")
+	names(cq) <- c("stats", "ASW")
+	if(any(xtabs(weights~clustering)<1)){
+		cq$ASW[, "ASW"] <- NA
+		cq$stats["ASW"] <- NA
+		warning(" [!] ASW can not be computed because at least one cluster has less than one observation.\n")
+	}
+	return(cq)
+#define ClusterQualHPG 0
+#define ClusterQualHG 1
+#define ClusterQualHGSD 2
+#define ClusterQualASWi 3
+#define ClusterQualASWw 4
+#define ClusterQualF 5
+#define ClusterQualR 6
+#define ClusterQualF2 7
+#define ClusterQualR2 8
+#define ClusterQualHC 9
+#define ClusterQualNumStat 10
+
+}
+
+wcSilhouetteObs <- function(diss, clustering, weights=NULL, measure="ASW"){
+	if(!all(measure %in% c("ASW", "ASWw"))){
+		stop(" [!] Unknow silhouette measure. It should be one of 'ASW', 'ASWw'.")
+	}
+	if (inherits(diss, "dist")) {
+		isdist <- TRUE
+		nelements <- attr(diss, "Size")
+	}else if(is.matrix(diss)){
+		isdist <- FALSE
+		nelements <- nrow(diss)
+		if(ncol(diss)!=nelements){
+			stop("[!] diss should be a squared matrix or a dist object.")
+		}
+	} else {
+		stop("[!] diss should be a squared matrix or a dist object.")
+	}
+	if(is.null(weights)){
+		weights <- as.double(rep(1.0, nelements))
+	}
+	clusterF <- factor(clustering)
+	if(nlevels(clusterF) < 2){
+		stop("[!] The clustering should have at least two different values.")
+	}
+	clustering <- as.integer(as.integer(clusterF)-1)
+	if(length(clustering)!=nelements|| length(weights)!=nelements){
+		stop("[!] different number of elements in diss, clustering and/or weights arguments.")
+	}
+	if(any(measure=="ASW") && any(xtabs(weights~clustering)<1)){
+		if(length(measure)==1){
+			stop(" [!] ASW can not be computed because at least one cluster has less than one observation. Use measure='ASWw'.")
+		}
+		warning(" [!] ASW can not be computed because at least one cluster has less than one observation. Only 'ASWw' is computed.")
+		measure <- "ASWw"
+	}
+	ncluster <- max(clustering)+1
+	ret <- .Call(wc_RClusterComputeIndivASW, diss, clustering, as.double(weights), as.integer(ncluster), as.integer(isdist))
+	asw <- data.frame(ASW=ret[[1]], ASWw=ret[[2]])
+	return(asw[, measure])
+	#define ClusterQualHPG 0
+#define ClusterQualHG 1
+#define ClusterQualHGSD 2
+#define ClusterQualASW 3
+#define ClusterQualF 4
+#define ClusterQualR 5
+#define ClusterQualF2 6
+#define ClusterQualR2 7
+}
+
+
+clusterqualitySimpleBoot <- function(diss, clustering, weights=NULL, R=999, replicates=TRUE, ...){
+	if (inherits(diss, "dist")) {
+		isdist <- TRUE
+		nelements <- attr(diss, "Size")
+	}else if(is.matrix(diss)){
+		isdist <- FALSE
+		nelements <- nrow(diss)
+		if(ncol(diss)!=nelements){
+			stop("[!] diss should be a squared matrix or a dist object.")
+		}
+	} else {
+		stop("[!] diss should be a squared matrix or a dist object.")
+	}
+	if(is.null(weights)){
+		weights <- as.double(rep(1.0, nelements))
+	}
+	clusterF <- factor(clustering)
+	if(nlevels(clusterF) < 2){
+		stop("[!] The clustering should have at least two different values.")
+	}
+	clustering <- as.integer(as.integer(clusterF)-1)
+	if(length(clustering)!=nelements|| length(weights)!=nelements){
+		stop("[!] different number of elements in diss, clustering and/or weights arguments.")
+	}
+	ncluster <- max(clustering)+1
+	totweights <- sum(weights)
+	fenv <- new.env()
+	fenv$R <- 0
+	if(replicates){
+		prob <- weights/totweights
+		sampleint <- as.integer(floor(totweights))
+		internalsample <- function(){
+			return(as.integer(sample.int(nelements, size=sampleint, replace=TRUE, prob=prob)-1L))
+		}
+		t <- .Call(wc_RClusterQualSimpleBoot, diss, clustering, as.double(weights), as.integer(ncluster), as.integer(R),  quote(internalsample()), environment(), sampleint)
+		colnames(t) <-c("PBC", "CH", "R2", "CHsq", "R2sq")
+		t0 <- t[1,]
+		bts <- list(t0=t0, t=t)
+	} else {
+		SimpleBootStatistic <- function(d, w) {
+			if(fenv$R==0){
+				w <- as.double(weights)
+				fenv$R <- 1
+			}else{
+				w <- as.double(w*totweights)
+			}
+			cq <- .Call(wc_RClusterQualSimple, diss, clustering, w, as.integer(ncluster))
+			names(cq) <-c("PBC", "HG", "HGSD", "ASW", "CH", "R2", "CHsq", "R2sq", "HC")
+			return(cq[c(1, 5:8)])
+		}
+		bts <- boot(clustering, stype="w", statistic=SimpleBootStatistic, weights=weights, R=R, ...)
+	}
+	return(bts)
+}
---
+++ R/kmedrange.R
@@ -0,0 +1,159 @@
+wcKMedRange <- function(diss, kvals, ...){
+	if (inherits(diss, "dist")) {
+		diss <- TraMineR:::dist2matrix(diss)
+	} else if(!is.matrix(diss)) {
+		diss <- as.matrix(diss)
+	}
+	n <- nrow(diss)
+	ret <- list()
+	ret$kvals <- kvals
+	ret$clustering <- matrix(-1, nrow=n, ncol=length(kvals))
+	ret$stats <-  matrix(-1, nrow=length(kvals), ncol=10)
+	i <- 1
+	for(k in kvals){
+		cl <- wcKMedoids(diss=diss, k=k, ...)
+		ret$clustering[,i] <- cl$clustering
+		ret$stats[i,] <- cl$stats
+		i <- i+1
+	}
+	ret$clustering <- as.data.frame(ret$clustering)
+	ret$stats <- as.data.frame(ret$stats)
+	colnames(ret$stats) <- names(cl$stats)
+	colnames(ret$clustering) <- paste("cluster", ret$kvals, sep="")
+	rownames(ret$stats) <- paste("cluster", ret$kvals, sep="")
+	class(ret) <- c("clustrange", class(ret))
+	return (ret)
+}
+
+as.clustrange <- function(object, diss, weights=NULL, ...){
+	UseMethod("as.clustrange")
+}
+
+as.clustrange.hclust <- function(object, diss, weights=NULL, ncluster, ...){
+	
+	if(ncluster<3){
+		stop(" [!] ncluster should be greater than 2.")
+	}
+	if (is.null(n1 <- nrow(object$merge)) || n1 < 1) {
+		stop("invalid 'object' (merge component)")
+	}
+    n <- n1 + 1
+	if(ncluster > n){
+		stop(" [!] ncluster should be less than ", n)
+	}
+	
+	pred <- data.frame(Split2=factor(cutree(object, 2)))
+	for(p in 3:ncluster){
+		pred[, paste("Split", p, sep="")] <- factor(cutree(object, p))
+	}
+	object <- pred
+	as.clustrange(object, diss=diss, weights=weights, ...)
+}
+
+as.clustrange.twins <- function(object, diss, weights=NULL, ncluster, ...) {
+	return(as.clustrange.hclust(object, diss=diss, weights=weights, ncluster=ncluster, ...))
+}
+
+as.clustrange.default <- function(object, diss, weights=NULL, ...){
+	ret <- list()
+	ret$clustering <- as.data.frame(object)
+	numclust <- ncol(ret$clustering)
+	ret$kvals <- numeric(numclust)
+	ret$stats <-  matrix(-1, nrow=numclust, ncol=10)
+	for(i in 1:numclust){
+		ret$kvals[i] <- length(unique(ret$clustering[,i]))
+		cl <- wcClusterQuality(diss, ret$clustering[,i], weights=weights)
+		ret$stats[i,] <- cl$stats
+	}
+	ret$stats <- as.data.frame(ret$stats)
+	colnames(ret$stats) <- names(cl$stats)
+	colnames(ret$clustering) <- paste("cluster", ret$kvals, sep="")
+	rownames(ret$stats) <- paste("cluster", ret$kvals, sep="")
+	class(ret) <- c("clustrange", class(ret))
+	return(ret)
+}
+print.clustrange <- function(x, digits=2, ...){
+	x <- round(x$stats, digits)
+	print(x, ...)
+}
+plot.clustrange <- function(x, stat="noCH", legendpos="bottomright", norm="none", withlegend=TRUE, lwd=1, col=NULL, ...){
+	kvals <- x$kvals
+	if(length(stat)==1){
+		if(stat=="all"){
+			stats <- x$stats
+			if(norm=="none"){
+				norm <- "range"
+			}
+		}else if(stat=="noCH"){
+			stats <- x$stats[,-grep("CH", colnames(x$stats))]
+		}
+		else{
+			stats <- x$stats[,stat]
+		}
+	}else{
+		if("RHC" %in% stat){
+			stats <- x$stats[ , stat[stat != "RHC"]]
+			stats[, "RHC"] <- 1 - x$stats[ , "HC"]
+		}
+		else{
+			stats <- x$stats[, stat]
+		}
+	}
+	ylim <- c(0,1)
+	if(norm == "range") {
+		for(i in 1:ncol(stats)){
+			stats[,i] <- (stats[,i]-min(stats[,i]))/(max(stats[,i])-min(stats[,i]))
+		}
+	} else if (norm=="zscore") {
+		for(i in 1:ncol(stats)){
+			stats[,i] <- (stats[,i]-mean(stats[,i]))/(sqrt(var(stats[,i])))
+		}
+		ylim <- range(unlist(stats), finite=TRUE)
+	}else if (norm=="zscoremed") {
+		for(i in 1:ncol(stats)){
+			stats[,i] <- (stats[,i]-median(stats[,i]))/(median(abs(stats[,i]-median(stats[,i]))))
+		}
+		ylim <- range(unlist(stats), finite=TRUE)
+	}
+	plot(kvals, xlim=range(kvals, finite=TRUE), ylim=ylim, type="n", ylab="Indicators", xlab="N clusters", ...)
+	labels <- character(ncol(stats))
+	if(is.null(col)){
+		allnames <- colnames(x$stats)
+		cols <- brewer.pal(length(allnames)+1, "Set3")[-2]
+		names(cols) <- allnames
+		cols["RHC"] <- cols["HC"]
+		cols <- cols[colnames(stats)]
+	} else {
+		if(length(col) < ncol(stats)){
+			stop(" [!] You should specify at least one color per quality measure to plot.")
+		}
+		cols <- col
+	}
+	for(i in 1:ncol(stats)){
+		ss <- stats[,i]
+		lines(kvals, ss, col=cols[i], lwd=lwd, ...)
+		labels[i] <- paste(colnames(stats)[i], "(", round(min(stats[,i]), 2),"/", round(max(stats[,i]),2), ")")
+	}
+	if(withlegend) {
+		legend(legendpos, fill=cols[1:ncol(stats)], legend=labels)
+	}
+}
+
+summary.clustrange <- function(object, max.rank=1, ...){
+	nstat <- ncol(object$stats)
+	clusterrank <- matrix(NA, nrow=nstat, ncol=max.rank*2)
+	rownames(clusterrank) <- colnames(object$stats)
+	clindices <- ((1:max.rank) *2) -1
+	valindices <- ((1:max.rank) *2)
+	cnames <- character(max.rank*2)
+	cnames[clindices] <- paste(1:max.rank, "N groups", sep=". ")
+	cnames[valindices] <- paste(1:max.rank, " stat", sep=". ")
+	colnames(clusterrank) <- cnames
+	for(s in colnames(object$stats)){
+		od <- order(object$stats[, s], decreasing=(s!="HC"))[1:max.rank]
+		clusterrank[s, clindices] <- object$kvals[od]
+		clusterrank[s, valindices] <- object$stats[od, s]
+	}
+	
+	return(as.data.frame(clusterrank, check.names=FALSE))
+}
